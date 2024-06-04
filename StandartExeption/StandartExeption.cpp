16dd711998e0b6419ff825906902d055baf53034
dutabrahmana <dutalinggau25@gmail.com>
2024-06-04 11:11:52 +0700

    menambahkan cout terkahir

--- StandartExeption/StandartExeption.cpp
+++ StandartExeption/StandartExeption.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     cout << "Awal Program" << endl; // penanda 1:...
     try {
-        array<int, 3> data = { 8, 4, 2 };
+        array<int, 3> data = { 1, 2, 3 };
         // pesan array integer 3 elemen
         cout << data.at(5) << endl;
     }
@@ -17,5 +17,8 @@ int main() {
         cout << e.what() << endl;
         /* akan dieksekusi karena array data hanya memiliki 3 elemen*/
     }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 
 }
