81e95c61ad87373c9148e0fa3c92c9aec6adcdc1
dutabrahmana <dutalinggau25@gmail.com>
2024-06-04 11:04:12 +0700

    menamahkan prosedure int main dan sekaligus menyelesaikan program

--- StandartExeption/StandartExeption.cpp
+++ StandartExeption/StandartExeption.cpp
@@ -5,3 +5,17 @@
 //untuk obyek array yang akan digunakan
 using namespace std;
 
+int main() {
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        // pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /* akan dieksekusi karena array data hanya memiliki 3 elemen*/
+    }
+
+}
