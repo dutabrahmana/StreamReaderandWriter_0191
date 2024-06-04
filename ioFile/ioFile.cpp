ee5e9de237e059a8a531c05f233be57ea10dc10f
dutabrahmana <dutalinggau25@gmail.com>
2024-06-04 10:13:22 +0700

    menambahkan ifstream, dan infile

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -23,4 +23,12 @@ int main() {
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
-   
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    
