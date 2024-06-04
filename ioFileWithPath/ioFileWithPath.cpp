bcbd7af676a8e2bc4e3e8f47f83c5e07b45505cf
dutabrahmana <dutalinggau25@gmail.com>
2024-06-04 10:35:10 +0700

    membuat offstream dan outfile

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -10,4 +10,9 @@ int main() {
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
 
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
    
