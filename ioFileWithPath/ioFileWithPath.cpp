c4c4afac75766ffef38a72500a97a97716dc2769
dutabrahmana <dutalinggau25@gmail.com>
2024-06-04 10:37:39 +0700

    menambahkan cout

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -27,4 +27,15 @@ int main() {
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+
+    // menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
     
